c5aa6f8e12c1252687ba5669c3dac029c9487cb4
BinhLD <8991nofear@gmail.com>
2024-04-03 22:13:46 +0700

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using API.DTOs;
+﻿using System.Security.Claims;
+using API.DTOs;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -28,5 +29,26 @@ public class UsersController : BaseApiController
     public async Task<ActionResult<MemberDto>> GetUser(string userName)
     {
         return await _userRepository.GetMembersByUserNameAsync(userName);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var userName = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var user = await _userRepository.GetUserByUserNameAsync(userName);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        _mapper.Map(memberUpdateDto, user);
+
+        if (await _userRepository.SaveAllAsync())
+        {
+            return NoContent();
+        }
+
+        return BadRequest("Failed to update user");
     }
 }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -8,17 +9,11 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent {
-  members: Member[] = [];
+  members$: Observable<Member[]> | undefined;
 
   constructor(private memberService: MembersService) {}
 
   ngOnInit() {
-    this.loadMember();
-  }
-
-  loadMember() {
-    this.memberService.getMembers().subscribe({
-      next: (members) => (this.members = members),
-    });
+    this.members$ = this.memberService.getMembers();
   }
 }
